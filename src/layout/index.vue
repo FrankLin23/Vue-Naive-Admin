8f88c8946a43a3408016ad2e6dc90ac3ab9a850c
FrankLin23 <linzhangsheng23@163.com>
2022-12-09 20:41:04 +0800

    chore: add router

---
+++ src/layout/index.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>Layout</template>
+
+<style scoped></style>
---
+++ src/router/index.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import type { RouteRecordRaw } from 'vue-router';
+
+export const constantRoutes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('@/layout/index.vue'),
+  },
+  {
+    path: '/login',
+    component: () => import('@/pages/Login.vue'),
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [],
+});
+
+export default router;
